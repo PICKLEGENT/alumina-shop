3edfc16d60d63c6311e08ecf3e3bea8ff710885b
Андрей Романов <human1488pwnz@yandex.ru>
2024-02-08 17:49:44 +0300

    Button Animation Done

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -2,7 +2,7 @@ import { CustomButtonProps } from "../types"
 
 const CustomButton = ({ title }: CustomButtonProps) => {
     return (
-        <button className="p-15 w-full font-medium text-lg text-white-100 tracking-widest uppercase cursor-pointer bg-red-100">{title}</button>
+        <button className="custom-btn__btn p-15 w-full font-medium text-lg text-white-100 tracking-widest uppercase cursor-pointer bg-red-100">{title}</button>
     )
 }
 
